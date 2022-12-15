58ae5601de5ae30962baad7ed4f91c2fe698002b
Jakub Płoskonka <ploskonkajakub@gmail.com>
2022-12-15 12:01:26 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `GatsbyAuth0Codecept`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
