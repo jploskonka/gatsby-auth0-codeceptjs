58be944fd6eeeec7ed33fbea521b1706b63a4d4f
Jakub Płoskonka <ploskonkajakub@gmail.com>
2022-12-21 09:21:16 +0100

    spec(e2e): add User authentication spec

---
+++ e2e/tests/User_authentication_test.js
@@ -0,0 +1,40 @@
+Feature('User authentication');
+
+// URLs that will be used in tests. I don't like using URLs directly in tests, for both readability and changeability reasons
+const rootUrl = '/'
+const afterLoginUrl = '/'
+const afterLogoutUrl = '/'
+
+// I'll need to have some user credentials to test authentication with. This may differ in dev/test environments so will be provided as environment variables
+const user = {
+  login: process.env.TEST_USER_LOGIN,
+  pass: process.env.TEST_USER_PASS
+}
+
+// Scenario messages in such a test are perfect documentation of the project. Don't be afraid to get verbose here.
+Scenario(`User logs in and lands on ${afterLoginUrl} page.`, ({ I }) => {
+  I.amOnPage(rootUrl)
+  I.click(`Log in`)
+  I.fillField(`username`, user.login)
+  I.fillField(`password`, secret(user.pass))
+  I.click(`Continue`)
+  I.waitForNavigation()
+
+  I.seeInCurrentUrl(afterLoginUrl)
+  I.see(user.login)
+})
+
+// One may say let's have this as one scenario where a user logs in, then logs out. It could work out, but if such a scenario fails it's hard to identify which part is broken.
+Scenario(`User logs out and lands on ${afterLogoutUrl}`, ({ I }) => {
+  I.amOnPage(rootUrl)
+	I.click(`Log in`)
+  I.fillField(`username`, user.login)
+  I.fillField(`password`, secret(user.pass))
+  I.click(`Continue`)
+  I.waitForNavigation()
+
+  I.click(`Log out`)
+  I.seeCurrentUrlEquals(afterLogoutUrl)
+  I.dontSee(user.login)
+  I.see(`Log in`)
+})
