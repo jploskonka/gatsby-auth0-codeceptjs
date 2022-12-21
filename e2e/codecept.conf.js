9ce5878996c96f843a030ca726802fe9101fec8c
Jakub Płoskonka <ploskonkajakub@gmail.com>
2022-12-21 09:20:54 +0100

    spec(e2e): setup codeceptJS

---
+++ e2e/codecept.conf.js
@@ -0,0 +1,23 @@
+const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure');
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
+setCommonPlugins();
+
+require('dotenv').config();
+
+exports.config = {
+  tests: './tests/*_test.js',
+  output: './output',
+  helpers: {
+    Playwright: {
+      url: 'http://localhost:8000',
+      show: true,
+      browser: 'chromium'
+    }
+  },
+  include: { },
+  name: 'e2e'
+}
