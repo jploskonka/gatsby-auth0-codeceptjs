9593b12d90c94deccec6cc3c1fb5c56db68b3f7b
Jakub Płoskonka <ploskonkajakub@gmail.com>
2022-12-21 14:38:01 +0100

    chore(e2e): add health_checks/auth_check.js

---
+++ e2e/codecept.prod.conf.js
@@ -0,0 +1,17 @@
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+setHeadlessWhen(process.env.HEADLESS);
+
+exports.config = {
+  tests: './health_checks/*_check.js',
+  output: './output',
+  helpers: {
+    Playwright: {
+      url: process.env.PROD_PAGE_URL,
+      show: true,
+      browser: 'chromium',
+      waitForTimeout: 5000,
+    }
+  },
+  include: { },
+  name: 'e2e'
+}
